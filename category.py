87a333029214558d459a1a283fbb6732ea4043ab
DivyaKaushik <divya.kaushik@accenture.com>
2017-06-26 19:07:57 -0700

    corrected typo - category

---
+++ category.py
@@ -0,0 +1,18 @@
+
+
+def create_category(base_cls):
+
+    class Category(base_cls):
+        
+        __tablename__ = 'category'
+        __table_args__ = {'autoload': True}
+
+        @property
+        def serialize(self):
+            """Return object data in easily serializeable format"""
+            return {
+                'Category_name': self.name,
+                'Category_id': self.id
+            }
+    
+    return Category
---
+++ category_items.py
@@ -0,0 +1,19 @@
+
+
+def create_category_items(base_cls):
+
+    class Category_items(base_cls):
+        
+        __tablename__ = 'category_items'
+        __table_args__ = {'autoload': True}
+    
+        @property
+        def serialize(self):
+            """Return object data in easily serializeable format"""
+            return {
+                'Category_items_name': self.name,
+                'Category_items_id': self.id,
+                'Category_items_description': self.description
+                }
+
+    return Category_items
